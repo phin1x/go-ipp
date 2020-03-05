2357cc318bb8fed14d9d936f8081e4756f90a564
Fabian <46140201+phin1x@users.noreply.github.com>
2020-03-05 16:29:09 +0100

    Added request decoder

--- reqest.go
+++ reqest.go
@@ -7,8 +7,11 @@ import (
 )
 
 type Request struct {
+	ProtocolVersionMajor uint8
+	ProtocolVersionMinor uint8
+
 	Operation Operation
-	RequestID int
+	RequestId int32
 
 	OperationAttributes map[string]interface{}
 	JobAttributes       map[string]interface{}
@@ -18,9 +21,17 @@ type Request struct {
 	FileSize int
 }
 
-func NewRequest(op Operation, reqID int) *Request {
+func NewRequest(op Operation, reqID int32) *Request {
 	return &Request{
-		op, reqID, make(map[string]interface{}), make(map[string]interface{}), make(map[string]interface{}), nil, -1,
+		ProtocolVersionMajor: ProtocolVersionMajor,
+		ProtocolVersionMinor: ProtocolVersionMinor,
+		Operation:            op,
+		RequestId:            reqID,
+		OperationAttributes:  make(map[string]interface{}),
+		JobAttributes:        make(map[string]interface{}),
+		PrinterAttributes:    make(map[string]interface{}),
+		File:                 nil,
+		FileSize:             -1,
 	}
 }
 
@@ -28,11 +39,11 @@ func (r *Request) Encode() ([]byte, error) {
 	buf := new(bytes.Buffer)
 	enc := NewAttributeEncoder(buf)
 
-	if err := binary.Write(buf, binary.BigEndian, ProtocolVersionMajor); err != nil {
+	if err := binary.Write(buf, binary.BigEndian, r.ProtocolVersionMajor); err != nil {
 		return nil, err
 	}
 
-	if err := binary.Write(buf, binary.BigEndian, ProtocolVersionMinor); err != nil {
+	if err := binary.Write(buf, binary.BigEndian, r.ProtocolVersionMinor); err != nil {
 		return nil, err
 	}
 
@@ -40,7 +51,7 @@ func (r *Request) Encode() ([]byte, error) {
 		return nil, err
 	}
 
-	if err := binary.Write(buf, binary.BigEndian, int32(r.RequestID)); err != nil {
+	if err := binary.Write(buf, binary.BigEndian, r.RequestId); err != nil {
 		return nil, err
 	}
 
@@ -91,4 +102,111 @@ func (r *Request) Encode() ([]byte, error) {
 	}
 
 	return buf.Bytes(), nil
+}
+
+type RequestDecoder struct {
+	reader io.Reader
+}
+
+func NewRequestDecoder(r io.Reader) *RequestDecoder {
+	return &RequestDecoder{
+		reader: r,
+	}
+}
+
+func (d *RequestDecoder) Decode(data io.Writer) (*Request, error) {
+	req := new(Request)
+
+	if err := binary.Read(d.reader, binary.BigEndian, req.ProtocolVersionMajor); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Read(d.reader, binary.BigEndian, req.ProtocolVersionMinor); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Read(d.reader, binary.BigEndian, req.Operation); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Read(d.reader, binary.BigEndian, req.RequestId); err != nil {
+		return nil, err
+	}
+
+	startByteSlice := make([]byte, 1)
+
+	tag := TagCupsInvalid
+	previousAttributeName := ""
+	tagSet := false
+
+	attribDecoder := NewAttributeDecoder(d.reader)
+
+	// decode attribute buffer
+	for {
+		if _, err := d.reader.Read(startByteSlice); err != nil {
+			return nil, err
+		}
+
+		startByte := startByteSlice[0]
+
+		// check if attributes are completed
+		if startByte == TagEnd {
+			break
+		}
+
+		if startByte == TagOperation {
+			tag = TagOperation
+			tagSet = true
+		}
+
+		if startByte == TagJob {
+			tag = TagJob
+			tagSet = true
+		}
+
+		if startByte == TagPrinter {
+			tag = TagPrinter
+			tagSet = true
+		}
+
+		if tagSet {
+			if _, err := d.reader.Read(startByteSlice); err != nil {
+				return nil, err
+			}
+			startByte = startByteSlice[0]
+		}
+
+		attrib, err := attribDecoder.Decode(Tag(startByte))
+		if err != nil {
+			return nil, err
+		}
+
+		if attrib.Name != "" {
+			appendAttributeToRequest(req, tag, attrib.Name, attrib.Value)
+			previousAttributeName = attrib.Name
+		} else {
+			appendAttributeToRequest(req, tag, previousAttributeName, attrib.Value)
+		}
+
+		tagSet = false
+	}
+
+	if data != nil {
+		if _, err := io.Copy(data, d.reader); err != nil {
+			return nil, err
+		}
+	}
+
+	return req, nil
+}
+
+func appendAttributeToRequest(req *Request, tag Tag, name string, value interface{}) {
+	switch tag {
+	case TagOperation:
+		req.OperationAttributes[name] = value
+	case TagPrinter:
+		req.PrinterAttributes[name] = value
+	case TagJob:
+		req.JobAttributes[name] = value
+	}
 }
